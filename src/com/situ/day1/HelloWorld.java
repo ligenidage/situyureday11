1980ba67702c54be91cb01c0b82931662bf86da3
lige <240193651@qq.com>
2017-05-08 14:56:08 +0800

    init project

---
+++ src/com/situ/day1/HelloWorld.java
@@ -0,0 +1,65 @@
+package com.situ.day1;
+
+import org.junit.Test;
+
+public class HelloWorld {
+	public static void main(String[] args){
+		System.out.println("helloworld!");
+        //num1是为变量标识符  字母，数字，下划线   但不能为关键字和数字开头
+		int num_1 = 80;
+		num_1 = 90;
+		int num_2 = 100;
+		int result = num_1 + num_2;
+		System.out.println(result);
+	}
+	
+	@Test
+	public void change() {
+		int num1 = 5;
+		int num2 = 3;
+		System.out.println("before: " + num1);
+		System.out.println("before: " + num2);
+		// 交换两个变量的值
+		int temp = num1;
+		num1 = num2;
+		num2 = temp;
+
+		System.out.println("after: " + num1);
+		System.out.println("after: " + num2);
+	}
+	
+	@Test
+	public void test1(){
+		//+-*/%
+		int num1 = 4;
+		int num2 = 10;
+		int result0 = num1 + num2;
+		int result = 10 / 4;
+		int result1 = 10 % 4;
+		System.out.println("result0" + result0);
+		System.out.println("result:" + result);
+		System.out.println("result1:" + result1 );
+	}
+	
+	@Test
+	public void test2(){
+		int year = 2017;
+		if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
+			System.out.println(year + "年 是闰年");
+		} else{ 
+			System.out.println(year + "年 不是闰年");
+			}
+	}
+	
+	@Test
+	public void year(){
+		int year = 1900;
+		for(;year < 2017 ;year++){
+			if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
+				System.out.println(year + "年是闰年");
+			}
+		}
+	}
+}
+
+
---
+++ src/com/situ/day1/Runnian.java
@@ -0,0 +1,46 @@
+package com.situ.day1;
+
+import java.util.Scanner;
+
+import org.junit.Test;
+
+public class Runnian {
+	public static void main(String[] args) {
+		int year = 2030;
+		if((year % 4 == 0 && year % 100 != 0) 
+				|| (year % 400 == 0)){
+			System.out.println(year + "年 是闰年");
+		} else {
+			System.out.println(year + "年 不是闰年");
+		}	
+	}
+		
+	@Test
+	public void xueji() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("请输入学籍成绩：");
+		int score = scanner.nextInt();
+		System.out.println("score:" + score);
+		if(score < 0 || score > 100){
+			System.out.println("无效");
+			return;
+		}
+		
+		if(score >= 90 && score <= 100) {
+			
+			
+			System.out.println("优秀");
+		} else if(score >= 80 ) {
+			System.out.println("良好");
+		} else if(score >= 70 )  {
+			System.out.println("一般");
+		} else if(score >= 60 ) {
+			System.out.println("及格");
+		} else if(score < 60) {
+			System.out.println("不及格");
+		} 
+	}
+	
+	
+	}
+}
